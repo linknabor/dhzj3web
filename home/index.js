145a06d81c194cfed76685562bb9b780b5dcc23a
jackie <liuzj_jj@163.com>
2017-08-22 18:24:06 +0800

    便民信息权限调整

--- home/index.js
+++ home/index.js
@@ -5,19 +5,28 @@ avalon.ready(function() {
 			o.banners = n.result.banners;
 			o.jingxuan1=n.result.jingxuan1;
 			o.jingxuan2=n.result.jingxuan2;
-			o.jingxuan3=n.result.jingxuan3;
 			initSwiper();
 		},function(){
 			alert("页面获取信息错误，请稍后重试！");
 		})
 	}
+	
+	function queryMessage(){
+		common.invokeApi("GET","getmessages",null,null,function(n){
+			o.jingxuan3=n.result.smallImage;
+			initSwiper();
+		},function(){
+			alert("页面获取信息错误，请稍后重试！");
+		})
+	}
+	
     var o = avalon.define({
         $id: "root",
         
         banners:[],
         jingxuan1:{},
         jingxuan2:{},
-        jingxuan3:{},
+        jingxuan3:'',
         gotoPage:function(url){
         	if(url =='')
         	{
@@ -33,7 +42,12 @@ avalon.ready(function() {
         	{
         		location.href="hotel.html";
         	}else if(type=="3"){
-        		location.href="../wuye/message.html?messageId=33";
+        		if(o.jingxuan3=='')
+        		{
+        			alert("未发布联络信息！");
+        			return;
+        		}
+        		location.href=o.jingxuan3;
         	}else
         	{
         		location.href="build.html";
@@ -42,6 +56,7 @@ avalon.ready(function() {
         }
     });
 	query();
+	queryMessage();	
     initWechat(['onMenuShareTimeline','onMenuShareAppMessage']);
     initShareConfig("鲜花、汽车、健康、维修、洗衣、家政...点亮生活，尽在东湖e家园管家服务！",MasterConfig.C("basePageUrl")+"home/index.html?v=20160229",MasterConfig.C("basePageUrl")+"/static/images/share_logo1.png","足不出户即享简单便捷的居家生活");
     avalon.scan(document.body),
