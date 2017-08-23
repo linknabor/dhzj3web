08642a79fe4599a534f22805b20627f6bfd6434a
jackie <liuzj_jj@163.com>
2017-08-23 20:47:24 +0800

    1213

--- home/index.js
+++ home/index.js
@@ -5,18 +5,7 @@ avalon.ready(function() {
 			o.banners = n.result.banners;
 			o.jingxuan1=n.result.jingxuan1;
 			o.jingxuan2=n.result.jingxuan2;
-			initSwiper();
-		},function(){
-			alert("页面获取信息错误，请稍后重试！");
-		})
-	}
-	
-	function queryMessage(){
-		common.invokeApi("GET","getmessages",null,null,function(n){
-			if(n.result!=null)
-			{
-				o.jingxuan3=n.result.smallImage;
-			}
+			o.jingxuan3=n.result.jingxuan3;
 			initSwiper();
 		},function(){
 			alert("页面获取信息错误，请稍后重试！");
@@ -45,12 +34,7 @@ avalon.ready(function() {
         	{
         		location.href="hotel.html";
         	}else if(type=="3"){
-        		if(o.jingxuan3=='')
-        		{
-        			alert("未发布联络信息！");
-        			return;
-        		}
-        		location.href=o.jingxuan3;
+        		location.href="../wuye/message.html";;
         	}else
         	{
         		location.href="build.html";
@@ -59,7 +43,6 @@ avalon.ready(function() {
         }
     });
 	query();
-	queryMessage();	
     initWechat(['onMenuShareTimeline','onMenuShareAppMessage']);
     initShareConfig("鲜花、汽车、健康、维修、洗衣、家政...点亮生活，尽在东湖e家园管家服务！",MasterConfig.C("basePageUrl")+"home/index.html?v=20160229",MasterConfig.C("basePageUrl")+"/static/images/share_logo1.png","足不出户即享简单便捷的居家生活");
     avalon.scan(document.body),
