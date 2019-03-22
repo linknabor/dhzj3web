92452a7a2255b106d379238763fce09f48d63870
zhaoshiwei <861812342.@qq.com>
2019-03-22 15:03:38 +0800

    增值个人中心

--- static/baseurl.js
+++ static/baseurl.js
@@ -1,29 +1,30 @@
+var url=/127|test/.test(location.origin)?'https://test.e-shequ.com':
+    /uat/.test(location.origin)?'https://uat.e-shequ.com':
+    'https://www.e-shequ.com';
+var name='/dhzj3';
+var namepay='/pay';
+
 window.baseurl = {
-    URL:'https://test.e-shequ.com/dhzj3/wechat/hexie/wechat/',
-    wuye:'https://test.e-shequ.com/dhzj3/weixin/wuye/index.html?c1234',
-    home:'https://test.e-shequ.com/dhzj3/weixin/home/index.html?v=20160229',
-    person:'https://test.e-shequ.com/dhzj3/weixin/person/index.html?v=20160229',
-
+    URL:url+name+'/wechat/hexie/wechat/',
+    wuye:url+name+'/weixin/wuye/index.html?c1234',
+    home:url+name+'/weixin/home/index.html?v=20160229',
+    person:url+name+'/weixin/person/index.html?v=20160229',
     //home
-    message:'https://test.e-shequ.com/dhzj3/weixin/wuye/index.html#/message"',
-    
+    message:url+name+'/weixin/wuye/index.html#/message"',
     //person
-    conpons:'https://test.e-shequ.com/dhzj3/weixin/group/onsalesindex.html',
-    orderpays:'https://test.e-shequ.com/pay/dhzj3orderpay.html?state=123#/repairPay?',//跳转支付
-    comment:'https://test.e-shequ.com/pay/dhzj3orderpay.html?state=123#/commentxiu',//跳转评论
-    order:'https://test.e-shequ.com/pay/dhzj3orderpay.html?start=123#/',//全部订单
-    grouporders:'https://test.e-shequ.com/pay/dhzj3orderpay.html?start=123#/grouporders',//团购订单
-    homeorders:'https://test.e-shequ.com/pay/dhzj3orderpay.html?start=123#/homeorders',//服务订单
-    baoxiu:'https://test.e-shequ.com/dhzj3/weixin/wuye/index.html?state=123#/publish?category=2',//报修
-    fuwux:'https://test.e-shequ.com/dhzj3/weixin/wuye/index.html?state=123#/butler?category=0',//服务需求 
-    butler:'https://test.e-shequ.com/dhzj3/weixin/wuye/index.html?state=123#/butler?category=1',//建议
-
-    //perhHifu
-    gotoGroupDetail:'https://test.e-shequ.com/dhzj3/weixin/group/rgroupinvite.html?',//团购详情
-    
-    // wuye
-    payment:"https://test.e-shequ.com/pay/dhzj3paymentdetail.html?#/?",//物业支付成功跳转
-
+    conpons:url+name+'/weixin/group/onsalesindex.html',
+    baoxiu:url+name+'/weixin/wuye/index.html?state=123#/publish?category=2',//报修
+    fuwux:url+name+'/weixin/wuye/index.html?state=123#/butler?category=0',//服务需求 
+    butler:url+name+'/weixin/wuye/index.html?state=123#/butler?category=1',//建议
+    gotoGroupDetail:url+name+'/weixin/group/rgroupinvite.html?',//团购详情
     //wuyezhifu
-    wuyezhi:'https://test.e-shequ.com/dhzj3/weixin/wuye/index.html?state=123#/payEnquiry'//成功后跳转
+    wuyezhi:url+name+'/weixin/wuye/index.html?state=123#/payEnquiry',//成功后跳转
+    //personzhi
+    orderpays:url+namepay+'/dhzj3orderpay.html?state=123#/repairPay?',//跳转支付
+    comment:url+namepay+'/dhzj3orderpay.html?state=123#/commentxiu',//跳转评论
+    order:url+namepay+'/dhzj3orderpay.html?start=123#/',//全部订单
+    grouporders:url+namepay+'/dhzj3orderpay.html?start=123#/grouporders',//团购订单
+    homeorders:url+namepay+'/dhzj3orderpay.html?start=123#/homeorders',//服务订单
+    // wuye
+    payment:url+namepay+'/dhzj3paymentdetail.html?#/?',//跳转物业支付
 }
