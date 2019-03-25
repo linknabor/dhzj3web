557d618399a9d5b3b2827a26a2b88c2dcd9bb335
zhaoshiwei <861812342.@qq.com>
2019-03-25 11:36:59 +0800

    物业测试修复1

--- wuye/static/baseurl.js
+++
@@ -1,9 +0,0 @@
-window.baseurl={
-    URL:"https://test.e-shequ.com/dhzj3/wechat/hexie/wechat/",//请求地址
-    wuye:"https://test.e-shequ.com/dhzj3/weixin/wuye/index.html",
-    home:"https://test.e-shequ.com/dhzj3/weixin/home/index.html",
-    person:"https://test.e-shequ.com/dhzj3/weixin/person/index.html",
-    payment:"https://test.e-shequ.com/dhzj3/weixin/dhzj3paymentdetail.html?#/?"//物业支付成功跳转
-}
-
-export default baseurl
